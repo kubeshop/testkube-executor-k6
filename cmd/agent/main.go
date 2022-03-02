ad2663c3183fa6b19f87630881e98bc53b9b3d50
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-03-02 12:49:06 +0100

    Adjusted Go module package.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/kubeshop/testkube-executor-template/pkg/runner"
+	"github.com/kubeshop/testkube-executor-k6/pkg/runner"
 	"github.com/kubeshop/testkube/pkg/executor/agent"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/kubeshop/testkube-executor-template
+module github.com/kubeshop/testkube-executor-k6
 
 go 1.17
 
